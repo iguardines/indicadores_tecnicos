df3b4e2f1cd8f8157c83e926c5e73fe0d59449d2
iguardines <iguardines@gmail.com>
2021-09-03 21:35:21 -0300

    Se agrega funcion media exponencial

--- indicadores.py
+++ indicadores.py
@@ -11,3 +11,8 @@ def devolver_top_n_variacion(df, n=10, es_de_baja=True):
   data["variacion"]  = data["Close"].pct_change() * 100 
   data.dropna(inplace=True)
   return data.sort_values("variacion", ascending = es_de_baja ).head(n)
+
+def agregar_media_exponencial(data, periodos):
+  df = data . copy()
+  df[f"ewm_{periodos}"] = df["Close"].ewm(span= periodos)
+  return df
